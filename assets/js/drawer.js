b1ef0ad798311da16d3f117d883c62eb3852f436
unknown <pflash90@gmail.com>
2022-07-15 10:15:05 +0100

    Refactor: Fixed the mobile menu to reflect same menu items as desktop and included dropdown capabilities.

--- assets/js/drawer.js
+++ assets/js/drawer.js
@@ -14,6 +14,10 @@
             this.closeDrawer = document.querySelector('#closeDrawer')
             this.body = document.querySelector('body')
 
+            // Drawer Menu Items with Dropdown
+            this.mobileMenuItemDropdownTrigger = document.querySelector('#mDropdownTrigger');
+            this.mobileMenuItemDropdownList = document.querySelector('#mDropdownList');
+
             view.render()
         },
         render: () => {
@@ -25,6 +29,11 @@
             this.hamburger.addEventListener('click', () => {
                 this.drawer.classList.toggle('drawerActive')
                 this.body.classList.toggle('noscroll')
+            }, false)
+
+            // Drawer Menu Items with Dropdown
+            this.mobileMenuItemDropdownTrigger.addEventListener('click', () => {
+                this.mobileMenuItemDropdownList.classList.toggle('active')
             }, false)
         }
     }
